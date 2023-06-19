5554c3b247011cfc0e381c6d0f9c674960d2ab9b
Brad Ziegler <bradleyziegler72@gmail.com>
2023-06-18 23:15:26 -0400

    Added ability to steer the car by touching the left or right side of the screen.

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class Car : MonoBehaviour {
     [SerializeField] private float speed = 1f;
     [SerializeField] private float speedGainOverTime = 1f;
+    [SerializeField] private float turnSpeed = 200f;
+
+    private int steerValue;
 
     void Update() {
         speed += speedGainOverTime * Time.deltaTime;
+
+        transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    public void Steer(int value) {
+        steerValue = value;
     }
 }
